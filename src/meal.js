6390f13540b63883328a254f6526ca8981c0cf04
Caitlin Cradick <caitlincradick@gmail.com>
2023-02-02 17:28:21 -0800

    I realized I forgot to remove skips from the test file for nameMenuItem

--- src/meal.js
+++ src/meal.js
@@ -1,11 +1,29 @@
 function nameMenuItem(name) {
-return `delicious ${name}`
+return `Delicious ${name}`
 }
 nameMenuItem('pizza')
 
+
+
+function createMenuItem (menuItemName, price, type) {
+  var menuItem = {
+    name: menuItemName,
+    price: price,
+    type: type,
+  }
+  return menuItem
+}
+createMenuItem ('burrito', 1.99, 'lunch')
+
+function addIngredients (ingredientName, numOfIngreditent) {
+var ingredients = [
+];
+} 
+
+
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
+  createMenuItem,
   // addIngredients,
   // formatPrice,
   // decreasePrice,
