0413414a23d30180ea27fe5accb2747dc51a8569
Caitlin Cradick <caitlincradick@gmail.com>
2023-02-04 10:50:51 -0800

    add completed section

--- src/meal.js
+++ src/meal.js
@@ -19,27 +19,37 @@ createMenuItem ('burrito', 1.99, 'lunch')
 function addIngredients (topping, ingredients) {
   if (!ingredients.includes(topping)) {
     ingredients.push(topping)
-    // console.log(ingredients)
+   
   }
 }
  function formatPrice (initialPrice) {
-  // console.log(initialPrice)
   var dollarSignPrice = `$${initialPrice}`
-  // dollarSignPrice.
   return dollarSignPrice
  }
   
-//  function decreasePrice (){
+ function decreasePrice (price) {
+  var price = 5.4 - 6 % 6.00 * 100
+ return price
+  }
 
-//  }
+function createRecipe (titleInput, ingredientInput, typeInput) {
+  var recipe = {
+    ingredients: ingredientInput,
+    title: titleInput,
+    type: typeInput
+  }
+  return recipe
+}
+
+
 
 module.exports = {
   nameMenuItem,
   createMenuItem,
   addIngredients,
   formatPrice,
-  // decreasePrice,
-  // createRecipe
+  decreasePrice,
+  createRecipe
 }
 
 
