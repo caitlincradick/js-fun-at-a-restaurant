53a170a334e9311cec7253ececc3ee23ef387fb8
Caitlin Cradick <caitlincradick@gmail.com>
2023-02-05 20:00:16 -0800

    add final results due date

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,36 @@
+function createRestaurant (restaurantName) {
+var restaurantNameArray ={
+  name:restaurantName,  
+  menus:
+  {
+  breakfast:[],
+  lunch: [],
+  dinner: []
+  }
+} 
+return restaurantNameArray
+}
+
+function addMenuItem(menusArray) {
+  // var menusArray = []
+  //   menus:
+  // {
+  //   name: [],  
+  //   price:[],
+  //   type: []
+  // }
+// for (i=0;menusArray.length; i++) {
+// pizzaRestaurant.menusArray.menus.lunch[i]+1
+// }
+// return menusArray 
+// }
+
+
+
 
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
+  createRestaurant, 
+  addMenuItem,
   // removeMenuItem
 }
